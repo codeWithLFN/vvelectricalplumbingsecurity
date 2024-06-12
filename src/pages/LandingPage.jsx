9baf68e7cc0b27c59f163eb9f180658c1f600da3
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2024-06-12 10:16:08 +0200

    update

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,7 +2,6 @@ import Header from '../components/Header.jsx';
 import HeroSection from '../components/HeroSection.jsx';
 import ServicesSection from '../components/ServicesSection.jsx';
 import AboutUsSection from '../components/AboutUsSection.jsx';
-import TestimonialsSection from '../components/TestimonialsSection.jsx';
 import PortfolioSection from '../components/PortfolioSection.jsx';
 import ContactSection from '../components/ContactSection.jsx';
 import FooterSection from '../components/FooterSection.jsx';
@@ -19,9 +18,6 @@ const LandingPage = () => (
     </div>
     <div id="services">
       <ServicesSection />
-    </div>
-    <div id="testimonials">
-      <TestimonialsSection />
     </div>
     <div id="projects">
       <PortfolioSection />
